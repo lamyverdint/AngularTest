c40af709adea87bfe190cc10253b2cbb7647b365
lamyvert <tiphanie.lamyverdin@edu.esiee.fr>
2021-03-27 23:10:52 +0100

    add zuul-project component

---
+++ app/src/app/zuul-project/zuul-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zuul-project',
+  templateUrl: './zuul-project.component.html',
+  styleUrls: ['./zuul-project.component.css']
+})
+export class ZuulProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
