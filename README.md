64e13df7a4d75068c68c94acf589c6ba2e4d634d
lamyvert <tiphanie.lamyverdin@edu.esiee.fr>
2021-03-29 11:34:19 +0200

    clean html and styles + readme update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AngularTest
 
 Attempt to create a website using Angular
+
+## Zuul project
+University first year project (2017) : create an adventure game using Java.
+[Git repository](https://github.com/lamyverdint/Java) of the Zuul project.
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ZuulProjectComponent } from './zuul-project/zuul-project.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZuulProjectComponent
+    ZuulProjectComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
