13e59cb4b6208a1fc5c1c438f52946dd086d6171
lamyvert <tiphanie.lamyverdin@edu.esiee.fr>
2021-03-30 11:09:54 +0200

    added navigation + main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ZuulProjectComponent } from './zuul-project/zuul-project.component';
@@ -12,7 +13,8 @@ import { MainPageComponent } from './main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
