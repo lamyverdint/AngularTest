43d6751058ebb0d98e6964fb5cbebfe210e1b8ac
Tiphanie LAMY VERDIN <tiphanie-externe.lamy-verdin@enedis.fr>
2022-02-21 18:12:51 +0100

    horizontal navigation bar

--- app/src/app/navigation-bar/navigation-bar.component.ts
+++ app/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  redirectToHomePage(): void {
+    this.router.navigate(['/']);
+  }
+
+  redirectToZuulProject(): void {
+    this.router.navigate(['/zuul-project']);
+  }
 }
